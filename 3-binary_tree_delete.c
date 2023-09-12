d8e6bd7ee84822af88b9c1e7dd713c79f55605d5
saima-riaz <saimariaz1422@gmail.com>
2023-09-12 09:08:18 -0700

    delete entire binary tree and fixed betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,21 +1,24 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_delete - delete tree
  * @tree: tree to delete
  * Return: void
  */
 
-void binary_tree_delete(binary_tree_t *tree) {
-	if (tree == NULL) {
-		return; // If tree is NULL, do nothing
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
 	}
 
-	// Delete the left subtree
+
 	binary_tree_delete(tree->left);
 
-	// Delete the right subtree
+
 	binary_tree_delete(tree->right);
 
-	// Free the memory of the current node
+
 	free(tree);
 }
