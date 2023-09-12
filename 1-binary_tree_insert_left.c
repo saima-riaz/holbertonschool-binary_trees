0b948f975359009b907519bab6e2ac6dfab375cf
saima-riaz <saimariaz1422@gmail.com>
2023-09-12 08:05:07 -0700

    task 1 to create left child and fixed betty

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,11 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_node - fun that create a node to binary tree
- *@parent: pointer to new node
- *@value: to put in the new node
+ *binary_tree_insert_left - fun that create a left child to binary tree
+ *@parent: pointer to new node to insert left child in
+ *@value: to put in the new left node
  *
- *Return: pointer to new node
+ *Return: new left node or NUll (if failed)
  */
 
 /* Function to insert a node as the left child of another node */
@@ -19,17 +19,18 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	/* Create a new node for the left child */
 	binary_tree_t *new_left_node = malloc(sizeof(binary_tree_t));
+
 	if (new_left_node == NULL)
 	{
-		return (NULL);  
+		return (NULL);
 		/* Memory allocation failed*/
 	}
 	/* Initialize the new node */
 	new_left_node->n = value;
 	new_left_node->parent = parent;
-	new_left_node->left = parent->left;   
+	new_left_node->left = parent->left;
 	/* Set the old left-child as the new node's left child */
-	new_left_node->right = NULL;          
+	new_left_node->right = NULL;
 	/* No right child for the new node */
 	if (parent->left != NULL)
 	{
@@ -38,5 +39,5 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	parent->left = new_left_node;
 	/* Set the new node as the left child of the parent */
-	return new_left_node;
+	return (new_left_node);
 }
