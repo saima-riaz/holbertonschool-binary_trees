0f7b7c05ddea81f5f545ad941d32ec87289db59c
saima-riaz <saimariaz1422@gmail.com>
2023-09-12 06:24:01 -0700

    fix main h prototype problem

--- binary_trees.h
+++ binary_trees.h
@@ -24,9 +24,11 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 //Print function//
-static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
