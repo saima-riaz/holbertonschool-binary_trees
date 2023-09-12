a2d1116856cecd2016dd32421319f1c8bf9909c8
seeyou7 <>
2023-09-12 06:43:01 -0700

    fix betty error

--- binary_trees.h
+++ binary_trees.h
@@ -17,17 +17,17 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
-//Print function//
+
 void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
@@ -52,7 +52,4 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-
-
-#endif 
+#endif
