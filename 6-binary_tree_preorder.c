7188e4b2d3b7dac5bb0e67c58914d32c1630865b
seeyou7 <>
2023-09-13 03:49:27 -0700

    task -6 fix betty error for TRAVERSAL IN PRE_ORDER of binary tree

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
+
 /**
-<<<<<<< HEAD
  * binary_tree_preorder - Perform pre-order traversal on a binary tree.
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
@@ -11,28 +11,10 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
 	{
-		return;  
+		return;
 	}
 
 	func(tree->n);
 
 	binary_tree_preorder(tree->left, func);
-
-=======
- * binary_tree_preorder - goes through a binary tree using pre-order traversal
- * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node
- *
- * Return: nothing
- */
-
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
-{
-	if (tree == NULL || func == NULL)
-		return;
-
-	func(tree->n);
-	binary_tree_preorder(tree->left, func);
->>>>>>> fca3b6464a972f43ca0d06a214a921452c44214e
 	binary_tree_preorder(tree->right, func);
-}
