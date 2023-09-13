021fbed831051d8692385549e1d29a2f2600f42b
saima-riaz <saimariaz1422@gmail.com>
2023-09-13 06:04:39 -0700

    spaces preferred error fixed

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,14 +13,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
 	{
-	return (0);
+		return (0);
 	}
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
 	{
-		return (left_height +1);
+		return (left_height + 1);
 	}
 	else
 		return (right_height + 1);
