7d78ed23011b1903532ea5fc86e3f25964e21a2c
seeyou7 <>
2023-09-13 01:18:55 -0700

    task -6 func to check if node is a root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_root - fun check if a node is a root
+ *@node: pointer to node to check
+ *
+ *Return: 1 if node is a root or 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node != NULL && node->parent == NULL)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
