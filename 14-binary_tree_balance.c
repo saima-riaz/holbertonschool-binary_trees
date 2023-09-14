7d5221d2f2ee6103ad73cf4a56ee00d7391c78cd
seeyou7 <>
2023-09-14 06:16:34 -0700

    fix balance func error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,30 +27,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_balance - Measure the balance factor of a binary tree.
- * @tree: A pointer to the root node of the tree to measure the balance factor.
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor
  *
- * Return: The balance factor of the tree. If tree is NULL, return 0.
+ * Return: balance factor of the tree or 0 if tree is NULL
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
 	if (tree == NULL)
 	{
-		return (0);
+	return (0);
 	}
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height > right_height)
-	{
-		return ((int)(left_height - right_height));
-	}
-	else
-	{
-		return ((int)(right_height - left_height));
-	}
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
