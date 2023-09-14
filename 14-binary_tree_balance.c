4e39eeea03d95975a297b48363fd10d24e5d7746
seeyou7 <>
2023-09-14 06:11:02 -0700

    task-14 fun to measures the balance factor of a binary

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -35,6 +35,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	size_t left_height;
+	size_t right_height;
 	if (tree == NULL)
 	{
 		return (0);
