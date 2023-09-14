43b6ca22dd9b4e236115fee420223696de83c321
seeyou7 <>
2023-09-14 06:27:01 -0700

    task-14 fun to measures the balance factor of a binary

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,24 +8,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	size_t left_height, right_height;
 
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+	if (tree == NULL)
 	{
-		return (0);
+	return (0);
 	}
+
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-	{
-		return (left_height + 1);
-	}
-	else
-		return (right_height + 1);
+	return ((left_height > right_height ? left_height : right_height) + 1);
 }
-
 /**
  * binary_tree_balance - measures the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
@@ -34,12 +28,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-<<<<<<< HEAD
-=======
-	size_t left_height;
-	size_t right_height;
-
->>>>>>> 429bbd72bfd95b80c4c020964c7cf2377943a624
 	if (tree == NULL)
 	{
 	return (0);
