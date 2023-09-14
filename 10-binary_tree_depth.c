0970b2c14365c34489d70d6f3c6da5d1c36b7221
saima-riaz <saimariaz1422@gmail.com>
2023-09-14 03:17:15 -0700

    fixed code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,9 +9,13 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+}
 
-	if (tree->parent == NULL)
-		return (0);
+while (tree->parent != NULL)
+{
+	depth++;
+	tree = tree->parent;
+}
 
-	return (1 + binary_tree_depth(tree->parent));
+return depth;
 }
